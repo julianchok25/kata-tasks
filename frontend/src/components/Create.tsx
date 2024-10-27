eac8ade7ee87eafb1870ea0dca582401a457f93c
Julian Villegas <jville7@bancodebogota.com.co>
2024-10-27 00:05:55 -0500

    [UPD] Validate create form

--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -7,6 +7,22 @@ function Create(props: {onCreate: () => void}) {
 
   const [title, setTitle] = useState<String | any>('');
   const [description, setDescription] = useState<String | any>('');
+  const [error, setError] = useState({titleError: '', descriptionError: ''});
+
+  const handleTitleChange = (e: any) => {
+    const {value} = e.target;
+    setTitle(value);
+
+    // Validate if the input is not empty
+    setError({titleError: !value.trim() ? 'Title is required.' : '', descriptionError: ''});
+  };
+
+  const handleDescChange = (e: any) => {
+    const {value} = e.target;
+    setDescription(value);
+
+    setError({titleError: '', descriptionError: !value.trim() ? 'Description is required.' : ''});
+  };
 
   const handleCreateTask = () => {
     axios.post('http://localhost:3000/tasks', {
@@ -26,14 +42,20 @@ function Create(props: {onCreate: () => void}) {
     .catch(error => {
       console.error(error);
     });
-
   }
 
   return (
     <article className="create-form">
       <h2 className="create-form__title">Create a new task</h2>
-      <input type="text" name='task-title' value={title} placeholder='Task title' onChange={(e) => setTitle(e.target.value)} />
-      <input type="text" name='task-description' value={description} placeholder='Description' onChange={(e) => setDescription(e.target.value)} />
+
+      <label htmlFor="task-title">Task Title <span>*</span></label>
+      <input className={error.titleError ? 'border-error': ''} type="text" name='task-title' value={title} placeholder='Task title' onChange={(e) => handleTitleChange(e)} />
+      {error.titleError && <span className='create-form--error'>{error.titleError}</span>}
+
+      <label htmlFor="task-title">Description <span>*</span></label>
+      <input  className={error.descriptionError ? 'border-error': ''} type="text" name='task-description' value={description} placeholder='Description' onChange={(e) => handleDescChange(e)} />
+      {error.descriptionError && <span className='create-form--error'>{error.descriptionError}</span>}
+  
       <button type='button' onClick={handleCreateTask}>Add</button>
     </article>
   )
